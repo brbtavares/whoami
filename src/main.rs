0e8b180804b4ac8f0abe60c83d444f299f2d01d0
brbtavares <brbtavares@outlook.com>
2024-11-14 17:06:30 -0300

    Add characteristics enums and characters struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
     println!("Hello, world!");
 }
+
+enum Cabelo_cor {
+    preto,
+    loiro,
+    ruivo,
+    castanho,
+}
+
+enum Pele_tonalidade {
+    branca,
+    parda,
+    negra,
+}
+
+enum Sexo {
+    masculino,
+    feminino,
+}
+
+struct Personagem {
+    cabelo_cor: Cabelo_cor,
+    pele_tonalidade: Pele_tonalidade,
+    sexo: Sexo,
+    franja: bool,
+}
