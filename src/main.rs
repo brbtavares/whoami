f54fbe14a0c55118fdf19db33f769ae582397ed9
brbtavares <brbtavares@outlook.com>
2024-11-16 12:41:39 -0300

    Add criar_personagens() function and and logic

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,126 @@
+use rand::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    let personagens: Vec<Personagem> = criar_personagens();
+    let thread_rng = thread_rng();
+    let mut rng: ThreadRng = thread_rng;
+    let l = i32::try_from(personagens.len()).unwrap();
+    let mut y = usize::try_from(rng.gen_range(0..l)).unwrap();
+    println!("O personagens escolhido foi {}.", personagens[y].nome);
 }
 
-enum Cabelo_cor {
-    preto,
-    loiro,
-    ruivo,
-    castanho,
+fn criar_personagens() -> Vec<Personagem> {
+    let v = vec![
+        Personagem {
+            nome: String::from("SALLY"),
+            cabelo_cor: CabeloCor::Castanho,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("MEG"),
+            cabelo_cor: CabeloCor::Ruivo,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: true,
+        },
+        Personagem {
+            nome: String::from("KATE"),
+            cabelo_cor: CabeloCor::Preto,
+            pele_tonalidade: PeleTonalidade::Parda,
+            sexo: Sexo::Feminino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("JOSH"),
+            cabelo_cor: CabeloCor::Loiro,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Masculino,
+            franja: true,
+        },
+        Personagem {
+            nome: String::from("KEVIN"),
+            cabelo_cor: CabeloCor::Castanho,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Masculino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("JULIA"),
+            cabelo_cor: CabeloCor::Preto,
+            pele_tonalidade: PeleTonalidade::Parda,
+            sexo: Sexo::Feminino,
+            franja: true,
+        },
+        Personagem {
+            nome: String::from("LAURA"),
+            cabelo_cor: CabeloCor::Loiro,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: true,
+        },
+        Personagem {
+            nome: String::from("JESS"),
+            cabelo_cor: CabeloCor::Preto,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("PAT"),
+            cabelo_cor: CabeloCor::Ruivo,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("ALEX"),
+            cabelo_cor: CabeloCor::Loiro,
+            pele_tonalidade: PeleTonalidade::Branca,
+            sexo: Sexo::Feminino,
+            franja: true,
+        },
+        Personagem {
+            nome: String::from("SARA"),
+            cabelo_cor: CabeloCor::Preto,
+            pele_tonalidade: PeleTonalidade::Parda,
+            sexo: Sexo::Feminino,
+            franja: false,
+        },
+        Personagem {
+            nome: String::from("MIKE"),
+            cabelo_cor: CabeloCor::Preto,
+            pele_tonalidade: PeleTonalidade::Negra,
+            sexo: Sexo::Masculino,
+            franja: false,
+        },
+    ];
+
+    v
 }
 
-enum Pele_tonalidade {
-    branca,
-    parda,
-    negra,
+enum CabeloCor {
+    Preto,
+    Loiro,
+    Ruivo,
+    Castanho,
+}
+
+enum PeleTonalidade {
+    Branca,
+    Parda,
+    Negra,
 }
 
 enum Sexo {
-    masculino,
-    feminino,
+    Masculino,
+    Feminino,
 }
 
 struct Personagem {
-    cabelo_cor: Cabelo_cor,
-    pele_tonalidade: Pele_tonalidade,
+    nome: String,
+    cabelo_cor: CabeloCor,
+    pele_tonalidade: PeleTonalidade,
     sexo: Sexo,
     franja: bool,
 }
